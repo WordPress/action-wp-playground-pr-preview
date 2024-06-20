0490b1646c7db1303c8d0b0aeb0387356a3a21e8
Vicente Canales <1157901+vcanales@users.noreply.github.com>
2024-06-20 15:05:12 -0400

    update bundle

--- dist/index.js
+++ dist/index.js
@@ -29353,7 +29353,7 @@ function getThemeDetails(dirName) {
     (0, core_1.debug)(`Reading ${styleCssPath}`);
     const content = fs.readFileSync(styleCssPath, 'utf-8');
     const themeNameMatch = content.match(/^Theme Name:\s*(.+)$/m);
-    const parentThemeMatch = content.match(/^Template:\s*(.+)$/m);
+    const parentThemeMatch = content.match(/^Template:\s*(\S+)$/m);
     const themeName = themeNameMatch ? themeNameMatch[1].trim() : '';
     const parentTheme = parentThemeMatch ? parentThemeMatch[1].trim() : null;
     (0, core_1.debug)(`Found themeName: ${themeName}, parentTheme: ${parentTheme}`);
