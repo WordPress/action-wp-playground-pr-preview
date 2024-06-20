21eb922202f0277f7146ceecd594ca7e91a45d0e
Vicente Canales <1157901+vcanales@users.noreply.github.com>
2024-06-20 12:17:35 -0400

    meta: add biomejs formatter

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,30 @@
-import * as core from '@actions/core';
-import * as github from '@actions/github';
+import { getInput, setFailed } from '@actions/core';
+import { context, getOctokit } from '@actions/github';
 import { exec } from '@actions/exec';
 
 export async function run() {
-  try {
-    const token = core.getInput('github-token', { required: true });
-    const octokit = github.getOctokit(token);
-    // Get org and repo names from context
-    const org = github.context.repo.owner;
-    const repo = github.context.repo.repo;
+	try {
+		const token = getInput('github-token', { required: true });
+		const octokit = getOctokit(token);
+		// Get org and repo names from context
+		const org = context.repo.owner;
+		const repo = context.repo.repo;
 
-    // Clone the repository
-    await exec('git', ['clone', '--depth=1', '--branch', 'trunk', `https://github.com/${org}/${repo}.git`]);
+		// Clone the repository
+		await exec('git', [
+			'clone',
+			'--depth=1',
+			'--branch',
+			'trunk',
+			`https://github.com/${org}/${repo}.git`,
+		]);
 
-    // Add more steps as necessary, replicating the original YAML workflow
-  } catch (error) {
-    core.setFailed(error.message);
-  }
+		// Add more steps as necessary, replicating the original YAML workflow
+	} catch (error: unknown) {
+		if (error instanceof Error) {
+			return setFailed(error.message);
+		}
+
+		return setFailed('An unexpected error occurred');
+	}
 }
