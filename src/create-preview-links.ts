3327c8b56c7de9fbe20032a8814641048e94b171
Juan Aldasoro <juanfraa@gmail.com>
2024-10-18 18:44:29 +0200

    Fix branch sanitization.

--- src/create-preview-links.ts
+++ src/create-preview-links.ts
@@ -67,7 +67,10 @@ function createBlueprint(
 	/* If themeDir is not provided, we assume that the action is running in a single theme workflow and the theme folder name will be the theme slug + the branch name.
 	 * If themeDir is provided, we assume that the action is running in a multi theme workflow and the theme folder name will be the theme slug.
 	 */
-	const themeFolderName = !themeDir ? `${themeSlug}-${branch}` : themeSlug;
+	const sanitizedBranch = branch.replace(/[\/\\:*?"<>|]/g, '-');
+	const themeFolderName = !themeDir
+		? `${themeSlug}-${sanitizedBranch}`
+		: themeSlug;
 	debug(`Theme folder name: ${themeFolderName}`);
 	const template: Template = {
 		steps: [
