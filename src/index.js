a9010aeaf8dcb0e47b9f091d727f2b3f56dcf0df
Adam Zieliński <adam@adamziel.com>
2026-05-28 19:27:19 +0100

    Fix template button diff-check whitespace

--- src/index.js
+++ src/index.js
@@ -206,7 +206,7 @@ const githubLib = require('@actions/github');
 
   	// Escape HTML entities somewhat naively to prevent the values leaking
   	// into HTML syntax elements.
-  	if(upperKey !== 'PLAYGROUND_BUTTON') {
+	  if (upperKey !== 'PLAYGROUND_BUTTON') {
   	  value = value
   		.replace(/&/g, '&amp;')
   		.replace(/</g, '&lt;')
